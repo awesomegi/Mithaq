a5a10e885beb545307cce4326f549cee3942164e
ghadi <ghadizarb@gmail.com>
2026-08-26 09:02:38 +0300

    update database to potgreSQL

--- Mithaq/settings.py
+++ Mithaq/settings.py
@@ -126,16 +126,11 @@ WSGI_APPLICATION = 'Mithaq.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
-    }
-} if not DEBUG else {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('PGDATABASE'),
+        'USER': config('PGUSER'),
+        'PASSWORD': config('PGPASSWORD'),
+        'HOST': config('PGHOST'),
+        'PORT': config('PGPORT', default='5432'),
     }
 }
 
